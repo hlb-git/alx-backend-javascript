238bc46f3cf5dae1acec432d10c07f44627d911b
hlb-git <mzeelovegeneral@gmail.com>
2024-07-25 21:42:15 +0100

    unittest with javascript chai and mocha

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,25 @@
+class Utils {
+    static calculateNumber(type, a, b) {
+	const first = Math.round(a);
+	const second = Math.round(b);
+	let result = 0;
+	switch (type) {
+	case 'SUM':
+            result = first + second;
+            break;
+	case 'SUBTRACT':
+            result = first - second;
+            break;
+	case 'DIVIDE':
+	    if (second === 0) {
+		result = "Error";
+	    } else {
+		result = first / second;
+	    }
+	    break;
+	}
+	return result;
+    }
+}
+
+module.exports = Utils;
